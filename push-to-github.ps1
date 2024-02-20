2bfc12b7908078da01e312e7d78440de9bc70270
Nigel van Keulen <nigel2392@gmail.com>
2024-02-20 12:22:25 +0100

    Update to package

--- push-to-github.ps1
+++ push-to-github.ps1
@@ -71,7 +71,7 @@ function GITHUB_NextVersion {
         git init
         git add .
         git branch -M "main"
-        git remote add origin "git@github.com/nigel2392/$(ProjectName).git"
+        git remote set-url origin "git@github.com:nigel2392/$(ProjectName).git"
         $newVersion = PYPI_NextVersion -ConfigFile $ConfigFile
         Write-Host "Next version (pypi): $newVersion"
     }
